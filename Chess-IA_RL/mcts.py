0dd2bbc6694693efce0887cd60ebbf60424e8def
Kiyogen <81473648+Reigen24@users.noreply.github.com>
2023-05-26 17:14:10 +0200

    Compatibilité Interface Graphique

--- Chess-IA_RL/mcts.py
+++ Chess-IA_RL/mcts.py
@@ -3,11 +3,11 @@ from math import log, inf, sqrt
 import random
 import chess
 import chess.engine
-import matplotlib.pyplot as plt
-
+import os
 
 #Stockfish
 engine = chess.engine.SimpleEngine.popen_uci(r"stockfish-windows-2022-x86-64-avx2.exe")
+
 
 
 # Définition de la classe Node
@@ -17,14 +17,14 @@ class Node:
         self.state = state
         # Parent du noeud
         self.parent = parent
-        # Liste des enfants du noeud
+        # Liste des coups possibles à partir de ce noeud
         self.children = []
         # Nombre de victoires du noeud
         self.wins = 0
         # Nombre de visites du noeud
         self.visits = 0
         
-    # Ajout d'un enfant à un noeud
+    # Ajout d'un child à un noeud
     def add_child(self, child_state):
         child = Node(child_state, self)
         self.children.append(child)
@@ -34,6 +34,9 @@ class Node:
     def update(self, reward):
         self.visits += 1
         self.wins += reward
+
+    
+
 
 
 def random_board(max_depth=100) :
@@ -71,7 +74,7 @@ def puct(node, c_param):
 
     for child in node.children:
         if child.visits == 0:
-            # Si l'enfant n'a pas encore été visité
+            # Si l'child n'a pas encore été visité
             exploitation_term = inf
             exploration_term = inf
         else: 
@@ -79,22 +82,20 @@ def puct(node, c_param):
             exploitation_term = child.wins / child.visits
             exploration_term = c_param * sqrt(log_total_visits / child.visits) * (1 / len(node.children))
         
-        # Calcul du score pour chaque enfant
+        # Calcul du score pour chaque child
         score = exploitation_term + exploration_term
         
-        # Mise à jour du meilleur score et de l'enfant correspondant
+        # Mise à jour du meilleur score et de l'child correspondant
         if score > best_score:
             best_score = score
             best_child = child
-
-    # Retourne l'enfant ayant le meilleur score
-    return best_child  
-    
+    # Retourne l'child ayant le meilleur score
+    return best_child
     
     
     
 # Évaluation de l'état de jeu
-def evaluate(state):
+def evaluate(state, player):
     piece_values = {'P': 1,'N': 3,'B': 3,'R': 5,'Q': 9,'K': 100,'p': 1,'n': 3,'b': 3,'r': 5,'q': 9,'k': 100}
     if state.is_checkmate():
         if " "+player+" " in str(state.fen()):
@@ -126,22 +127,27 @@ def evaluate(state):
             return score / 100.0
 
 
-# Sélection d'un noeud fils avec la plus grande valeur UCB/PUCT
-def select_node(node):
-       """max_score = -inf
-        best_child = None
-        for child in node.children:
-            score = ucb(child)
-            if score > max_score:
-                max_score = score
-                best_child = child
-        return best_child"""
-        return puct(node, 5.0)
+# Sélection d'un noeud fils avec la plus grande valeur UCB ou PUCT
+def select_node(node,state, player):
+    """
+    max_score = -inf
+    best_child = None
+    for child in node.children:
+        score = ucb(child)
+        if score > max_score:
+            max_score = score
+            best_child = child
+    return best_child"""
 
-# Expansion d'un noeud avec un mouvement aléatoire
-def expand_node(node, state):
+    return puct(node, 5.0)
+
+# Expansion d'un noeud avec un mouvement aléatoire si c'est l'algo et le meilleur coup si c'est le joueur
+def expand_node(node, state, player):
     if " "+player+" " in state.fen():
-        best_possible_move = engine.play(state, chess.engine.Limit(time=0.1,depth = 18))
+        try:
+            best_possible_move = engine.play(state, chess.engine.Limit(time=0.1,depth = 18))
+        except:
+            best_possible_move = random.choice(list(state.legal_moves))
         new_state = state.copy()
         new_state.push(best_possible_move.move)
         if new_state not in [child.state for child in node.children]:
@@ -155,39 +161,28 @@ def expand_node(node, state):
             if new_state not in [child.state for child in node.children]:
                 node.add_child(new_state)
         random_move = random.choice(node.children)
-        return random_move, 0
+        return random_move
 
 # Simulation d'un jeu jusqu'à la fin
-def simulate(state, after):   
-    if after == 1: #si le prochain coup est celui de l'algo
-        while not state.is_game_over():
-            #distance_checkmate = analyze_position(state) #Avec Stockfish
-            #state.push_san(distance_checkmate[0]["pv"][0])
-            state.push(random.choice(list(state.legal_moves))) # Avec des mouvements aléatoires
-            """best_possible_move = engine.play(state, chess.engine.Limit(time=0.1,depth = 18))
-            if state.is_game_over():
+def simulate(state, player): 
+    while not state.is_game_over():
+        state.push(random.choice(list(state.legal_moves))) # Avec des mouvements aléatoires
+        """best_possible_move = engine.play(state, chess.engine.Limit(time=0.1,depth = 18))
+        if state.is_game_over():
                 return evaluate(state)
-            else:
-                state.push(best_possible_move.move)"""
-        return evaluate(state)
-    else: #si c'est celui du joueur
-        while not state.is_game_over():
-            """best_possible_move = engine.play(state, chess.engine.Limit(time=0.1,depth = 18))
+        else:
             state.push(best_possible_move.move)"""
-            if state.is_game_over():
-                return evaluate(state)
-            else:
-                state.push(random.choice(list(state.legal_moves))) # Avec des mouvements aléatoires
-        return evaluate(state)
+    return evaluate(state, player)
+
 
 #Mise à jour de la valeur de tous les Noeuds
-def backpropagate(node, reward):
+def backpropagate(node, reward, player):
     while node is not None:
         node.update(reward)
         node = node.parent
         
 
-        
+
 def analyze_position(board, num_moves_to_return=1, depth_limit=10, time_limit=0.1):
     search_limit = chess.engine.Limit(depth=depth_limit, time=time_limit)
     infos = engine.analyse(board, search_limit, multipv=num_moves_to_return)
@@ -210,8 +205,9 @@ def format_moves(pv):
 
 
 
-def mcts(root, state, itermax):
+def mcts(root, state, itermax, player):
     """Monte Carlo Tree Search algorithme"""
+    print("loading...")
     distance_checkmate = analyze_position(state)
     if distance_checkmate[0]["mate_score"]!=None:
         return distance_checkmate[0]["pv"][0]
@@ -226,26 +222,28 @@ def mcts(root, state, itermax):
                 if new_state not in [child.state for child in node.children]:
                     node.add_child(new_state)
             first_expansion = False
-            
         while node.children:
-            node = select_node(node)
+            #print("1", "\n", node.state)
+            node = select_node(node,current_state, player)
             current_state.push(node.state.peek())
-                
+
         if not current_state.is_game_over():
-            node, after = expand_node(node, current_state)
-            reward = simulate(current_state, after)
-            backpropagate(node, reward)
+            #print("2", "\n", node.state)
+            node = expand_node(node, current_state, player)
+            reward = simulate(current_state, player)
+            backpropagate(node, reward, player)
         else:
-            reward = evaluate(current_state)
-            backpropagate(node, reward)
+            reward = evaluate(current_state, player)
+            backpropagate(node, reward, player)
     moves = {}
     for move in root.children:
-        moves[move.state.peek()] = (move.wins,move.visits)
+        moves[str(move.state.peek())] = (move.wins,move.visits)
     best_moves = dict(sorted(moves.items(), key=lambda item:item[1],reverse=True))
     for (cle, valeur) in enumerate(best_moves.items()):
         print(f"{cle}: {valeur}")
-   
-     return max(root.children, key=lambda x: x.wins / x.visits).state.peek()
+
+    #return select_node(root, board).state.peek()
+    return max(root.children, key=lambda x: x.visits).state.peek()
 
 
 """
@@ -257,9 +255,11 @@ if __name__ == "__main__":
     best_move = mcts(root, board, 1000)
     #print(board.fen())
     print(best_move)
-    print(board)"""
+    print(board)
+"""
 
 
+import matplotlib.pyplot as plt
 
 
 fig, ax = plt.subplots(figsize=(20, 10), dpi = 40)
@@ -267,9 +267,10 @@ ax.set_xlim(0, 10)
 ax.set_ylim(0, 10)
 
 def plot_node(node, x, y, dx, dy):
-    """Dessiner l'arbre des coups"""
+    # Plot the node
     ax.annotate(node.state, xy=(x, y), xytext=(x, y+0.5), ha='center', va='center', bbox=dict(boxstyle='square', facecolor='w', edgecolor='black'))
 
+    # Plot the edges to the children
     if node.children:
         if len(node.children) == 1:
             x_spacing = 0
@@ -282,29 +283,44 @@ def plot_node(node, x, y, dx, dy):
             y_child = y_start
             ax.plot([x, x_child], [y, y_child], 'k-', lw=1)
             plot_node(child, x_child, y_child, dx/2, dy)
-            
-            
-            
-    
+
+
+
+
+
+
+
+
 def move_player():
     try :
-        player_move = input("Move: ")
-        board.push_san(str(player_move))
+        player_move = str(input("Move: "))
+        if player_move == "s":
+            best_possible_move = engine.play(board, chess.engine.Limit(time=2,depth = 18))
+            board.push(best_possible_move.move)
+        else:
+            board.push_san(player_move)
     except:
         print("Invalid move")
         move_player()
 
 
-
+"""
 def play():
     global player
-    iterations = 1000
+    iterations = 100
     first = input("White or Black ? (w/b) ")
 
-        
+    new_root = None 
+
     if str(first)=="w" or str(first).lower=="white" or str(first).lower=="White":
         while not board.is_game_over():
-            root = Node(board) 
+            if new_root == None :
+                root = Node(board) 
+            else :
+                root = new_root
+                plot_node(root, 0, 0, 10, 5)
+                plt.axis('off')
+                plt.show()
             print(board.fen())
             print(board)
             player = "w"
@@ -330,13 +346,24 @@ def play():
                 print("Egalité") 
                 print(board)
                 break
-            """plot_node(root, 0, 0, 10, 5)
+            print("Here :")
+            print(root.state)
+            #new_root = dfs_search(root, board, root)
+        
+            plot_node(root, 0, 0, 10, 5)
             plt.axis('off')
-            plt.show()"""
+            plt.show()
 
     elif str(first)=="b" or str(first).lower=="black" or str(first).lower=="Black":
         while not board.is_game_over():
             root = Node(board) 
+            if os.path.isfile('mcts_tree.json'):
+                with open('mcts_tree.json', 'r') as f:
+                    data = json.load(f)
+                explored_tree = dict_vers_noeud(data)
+                new_root = recherche_valeur(explored_tree, str(root.state.fen()))
+                if new_root != None:
+                    root = dict_vers_noeud(new_root)
             print(board.fen())
             print(board)
             player = "b"
@@ -364,9 +391,10 @@ def play():
                 print("Egalité") 
                 print(board)
                 break
-            """plot_node(root, 0, 0, 10, 5)
+            plot_node(root, 0, 0, 10, 5)
             plt.axis('off')
-            plt.show()"""
+            plt.show()
+            
     else :
         print("Invalid input")
         play()
@@ -375,4 +403,5 @@ def play():
 
 
 board=chess.Board()
-play()
+play()"""
+board=chess.Board()
